0eae9729da3a53f9115ea7381e330cbb9c1e7ecb
Guillaume Comte <32095526+GuillaumeComte@users.noreply.github.com>
2017-10-12 00:34:49 +0200

    Add files via upload

--- tp-01/TaskManager/Sources/TaskManagerLib/TaskManagerLib.swift
+++ tp-01/TaskManager/Sources/TaskManagerLib/TaskManagerLib.swift
@@ -52,19 +52,33 @@ public func createCorrectTaskManager() -> PTNet {
         postconditions : [PTArc(place: processPool)])
     let success     = PTTransition(
         named          : "success",
-        preconditions  : [PTArc(place: taskPool), PTArc(place: inProgress)],
+        preconditions  : [PTArc(place: inProgress)],
         postconditions : [])
+    // On enlève la pré-condition taskPool de success pour qu'une execution
+    // puisse être réussi sans avoir un jeton dans taskPool 
     let exec       = PTTransition(
         named          : "exec",
         preconditions  : [PTArc(place: taskPool), PTArc(place: processPool)],
-        postconditions : [PTArc(place: taskPool), PTArc(place: inProgress)])
+        postconditions : [PTArc(place: inProgress)])
+    // j'ai retiré la postcondition taskPool qui permettait une tâches de
+    // s'executer plusieurs fois si on avait plusieurs processus et de ce fait
+    // laisser un jeton dans inProgress alors qu'il n'y a rien dans taskPool
+    // permettant de réussir l'execution.
+
     let fail        = PTTransition(
         named          : "fail",
         preconditions  : [PTArc(place: inProgress)],
-        postconditions : [])
+        postconditions : [PTArc(place: processPool), PTArc(place: taskPool)])
+
+    // Pour la transition fail j'ai ajouté 2 postcondition, 1 allant vers
+    // processPool, pour que le processus retourne dans
+    // l'ensemble des processus après que l'éxecution. De cette manière si
+    // l'exécution échoue il est possible de ré-executer la tâche.
+    // ET 1 allant à taskPool pour que la tâche qui n'a pas pu être executé
+    // retourne dans l'ensemble des tâches
 
     // P/T-net
     return PTNet(
-        places: [taskPool, processPool, inProgress],
-        transitions: [create, spawn, success, exec, fail])
+         places: [taskPool, processPool, inProgress],
+         transitions: [create, spawn, success, exec, fail])
 }
