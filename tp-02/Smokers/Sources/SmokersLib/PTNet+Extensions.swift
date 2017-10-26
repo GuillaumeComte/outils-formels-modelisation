8d406a7660edf741cc2f69fedb5794885107749d
Guillaume Comte <32095526+GuillaumeComte@users.noreply.github.com>
2017-10-26 21:47:16 +0200

    Add files via upload

--- tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
+++ tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
@@ -1,6 +1,5 @@
 import PetriKit
 
-
 public class MarkingGraph {
 
     public let marking   : PTMarking
@@ -53,149 +52,146 @@ public extension PTNet {
         return visited[0]
     }
 
-
-
-
-    public func seen(p: PTMarking, q: [MarkingGraph]) -> Bool {
-        // Cette fonction prend en paramètre un marquage "p" et un graphe de
-        // marquage "q". Si jamais le marquage se trouve dans le graphe de
-        // marquage, la fonction retourne 'false' pour dire que ce marquage n'est
+public func seen(p: PTMarking, q: [MarkingGraph]) -> Bool {
+        // Cette fonction prend en paramètre un marquage "p" et une liste de
+        // marquages "q". Si jamais le marquage se trouve dans la liste de
+        // marquages, la fonction retourne 'false' pour dire que ce marquage n'est
         // pas nouveau. Sinon elle retourne'true' pour dire qu'elle ne l'a jamais vu.
-        for n in q {
-            if(n.marking == p){
-                return false
-                // pour chaque marquage "n" dans le graphe de marquage
+    for n in q {
+        if(n.marking == p){
+            return false
+                // pour chaque marquage "n" dans la liste de marquages
                 // la boucle vérifie si "n" est égal à "p"
+        }
+    }
+    return true
+}
+
+
+
+
+public func showGraph(i: MarkingGraph){
+    // Cette fonction permet d'afficher le graphe de marquage.
+    var visited = [i]
+    // Les marquages déjà vus
+    var toVisit = [i]
+    // Les marquages dont on doit voir les successeurs
+    print("Nous obtenons le graphe de marquage suivant:")
+    while let present = toVisit.popLast(){
+        // On prend le dernier marquage du tableau des marquages à voir
+        for (transitions, successors) in present.successors {
+            // On regarde chaque transition et successeur du marquages selectionné
+            if !visited.contains(where: { $0 === successors}){
+                // Si la liste de marquages vu ne possède pas ce successeur
+                visited.append(successors)
+                // On l'ajoute dans la liste des marquages vus
+                toVisit.append(successors)
+                // On l'ajoute dans la liste des marquages dont il faut regarder
+                // les successeurs
+                print(transitions, "  ", successors.marking)
+                // On print la transition faite, ainsi que le marquage auqel
+                // aboutit la transition
+
             }
         }
-        return true
     }
+}
 
 
 
 
-    public func showGraph(i: MarkingGraph){
-        // Cette fonction permet d'afficher le graphe de marquage.
-        var visited = [i]
-        // Les marquages déjà vus
-        var toVisit = [i]
-        // Les marquages dont on doit voir les successeurs
-        print("Nous obtenons le graphe de marquage suivant:")
-        while let present = toVisit.popLast(){
-            // On prend le dernier marquage du tableau des marquages à voir
-            for (transitions, successors) in present.successors {
-                // On regarde chaque transition et successeur du marquages selectionné
-                if !visited.contains(where: { $0 === successors}){
-                    // Si la liste de marquages vu ne possède pas ce successeur
-                    visited.append(successors)
-                    // On l'ajoute dans la liste des marquages vus
-                    toVisit.append(successors)
-                    // On l'ajoute dans la liste des marquages dont il faut regarder
-                    // les successeurs
-                    print(transitions, "  ", successors.marking)
-                    // On print la transition faite, ainsi que le marquage auqel
-                    // aboutit la transition
+public func countNodes(markingGraph: MarkingGraph) -> Int {
+    // Cette fonction compte le nombre d'états différents que peut prendre le réseau.
+    var visited = [markingGraph]
+    // les marquages déjà vus
+    var toVisit = [markingGraph]
+    // les marquages dont on doit voir les successeurs
+    while let present = toVisit.popLast(){
+        // On prend un marquage dont on a pas encore regarder les successeurs
+        for (_, successors) in present.successors{
+            if !visited.contains(where: { $0 === successors}) {
+                visited.append(successors)
+                // Si le marquage successeur n'as jamais été vu on le mets
+                // dans la liste des marquages vu
+                toVisit.append(successors)
+                // et dans la liste des marquages dont on doit regarder les successeurs
+            }
+        }
+    }
+    return visited.count
+    // On compte le nombre total de marquages vus
 
+}
+
+
+
+
+public func twoSmokers(m0: MarkingGraph) -> Bool {
+    // Cette fonction retourne 'true' ou 'false' si il est possible
+    // ou non que deux fumeurs différents puissent fumer en même temps.
+    var visited = [m0]
+    // Les marquages déjà vus
+    var toVisit = [m0]
+    // Les marquages dont on doit voir les successeurs
+    while let present = toVisit.popLast(){
+        for (_, successors) in present.successors{
+            if !visited.contains(where: { $0 === successors}){
+                visited.append(successors)
+                toVisit.append(successors)
+                let place1 = self.places.first(where: { $0.name == "s1" })!
+                let place2 = self.places.first(where: { $0.name == "s2" })!
+                let place3 = self.places.first(where: { $0.name == "s3" })!
+                // Soit place1, 2 ,3 la place des fumeurs 1, 2 et 3
+                let smoker1 = (successors.marking[place1] != 0)
+                let smoker2 = (successors.marking[place2] != 0)
+                let smoker3 = (successors.marking[place3] != 0)
+                // Soit smoker 1, 2, 3 des boulléens qui retourne false ou true
+                // False si ils n'ont pas de jetons, true sinon.
+
+                if smoker1 && smoker2 || smoker1 && smoker3 || smoker3 && smoker2 {
+                    // si plus d'un fumeur possède un jeton (est en train de fumer)
+                    // la fonction sort true, sinon elle retourne false
+                    return true
                 }
             }
         }
     }
+    return false
+}
 
 
 
 
-    public func countNodes(markingGraph: MarkingGraph) -> Int {
-        // Cette fonction compte le nombre d'états différents que peut prendre le réseau.
-        var visited = [markingGraph]
-        // les marquages déjà vus
-        var toVisit = [markingGraph]
-        // les marquages dont on doit voir les successeurs
-        while let present = toVisit.popLast(){
-            // On prend un marquage dont on a pas encore regarder les successeurs
-            for (_, successors) in present.successors{
-                if !visited.contains(where: { $0 === successors}) {
-                    visited.append(successors)
-                    // Si le marquage successeur n'as jamais été vu on le mets
-                    // dans la liste des marquages vu
-                    toVisit.append(successors)
-                    // et dans la liste des marquages dont on doit regarder les successeurs
+public func sameIngredient(m0: MarkingGraph) -> Bool {
+    // Cette fonction retourne 'true' ou 'false' si il est possible
+    // ou non que deux fois le même ingrédient puisse se retrouver
+    // sur la table.
+    var visited = [m0]
+    // les marquages déjà vus
+    var toVisit = [m0]
+    // Les marquages dont on doit voir les successeurs
+    while let present = toVisit.popLast(){
+        for (_, successors) in present.successors{
+            if !visited.contains(where: { $0 === successors}){
+                visited.append(successors)
+                toVisit.append(successors)
+                let place1 = self.places.first(where: { $0.name == "p" })!
+                let place2 = self.places.first(where: { $0.name == "m" })!
+                let place3 = self.places.first(where: { $0.name == "t" })!
+                // Soit place 1 , 2 ,3 les places des ingrédients paper , matches
+                // et tabaco respectivement
+                let ingr1 = successors.marking[place1]
+                let ingr2 = successors.marking[place2]
+                let ingr3 = successors.marking[place3]
+                // Si un ingrédient possède plus d'un jeton la fonction
+                // retourne true, sinon false
+                if ingr1! > 1 || ingr2! > 1 || ingr3! > 1 {
+                    return true
                 }
             }
         }
-        return visited.count
-        // On compte le nombre total de marquages vus
-
     }
-
-
-
-
-    public func twoSmokers(m0: MarkingGraph) -> Bool {
-        // Cette fonction retourne 'true' ou 'false' si il est possible
-        // ou non que deux fumeurs différents puissent fumer en même temps.
-        var visited = [m0]
-        // Les marquages déjà vus
-        var toVisit = [m0]
-        // Les marquages dont on doit voir les successeurs
-        while let present = toVisit.popLast(){
-            for (_, successors) in present.successors{
-                if !visited.contains(where: { $0 === successors}){
-                    visited.append(successors)
-                    toVisit.append(successors)
-                    let place1 = self.places.first(where: { $0.name == "s1" })!
-                    let place2 = self.places.first(where: { $0.name == "s2" })!
-                    let place3 = self.places.first(where: { $0.name == "s3" })!
-                    // Soit place1, 2 ,3 la place des fumeurs 1, 2 et 3
-                    let smoker1 = (successors.marking[place1] != 0)
-                    let smoker2 = (successors.marking[place2] != 0)
-                    let smoker3 = (successors.marking[place3] != 0)
-                    // Soit smoker 1, 2, 3 des boulléens qui retourne false ou true
-                    // False si ils n'ont pas de jetons, true sinon.
-
-                    if smoker1 && smoker2 || smoker1 && smoker3 || smoker3 && smoker2 {
-                        // si plus d'un fumeur possède un jeton (est en train de fumer)
-                        // la fonction sort true, sinon elle retourne false
-                        return true
-                    }
-                }
-            }
-        }
-        return false
-    }
-
-
-
-
-    public func sameIngredient(m0: MarkingGraph) -> Bool {
-        // Cette fonction retourne 'true' ou 'false' si il est possible
-        // ou non que deux fois le même ingrédient puisse se retrouver
-        // sur la table.
-        var visited = [m0]
-        // les marquages déjà vus
-        var toVisit = [m0]
-        // Les marquages dont on doit voir les successeurs
-        while let present = toVisit.popLast(){
-            for (_, successors) in present.successors{
-                if !visited.contains(where: { $0 === successors}){
-                    visited.append(successors)
-                    toVisit.append(successors)
-                    let place1 = self.places.first(where: { $0.name == "p" })!
-                    let place2 = self.places.first(where: { $0.name == "m" })!
-                    let place3 = self.places.first(where: { $0.name == "t" })!
-                    // Soit place 1 , 2 ,3 les places des ingrédients paper , matches
-                    // et tabaco respectivement
-                    let ingr1 = successors.marking[place1]
-                    let ingr2 = successors.marking[place2]
-                    let ingr3 = successors.marking[place3]
-                    // Si un ingrédient possède plus d'un jeton la fonction
-                    // retourne true, sinon false
-                    if ingr1! > 1 || ingr2! > 1 || ingr3! > 1 {
-                        return true
-                    }
-                }
-            }
-        }
-        return false
-    }
+    return false
+}
 
 }
