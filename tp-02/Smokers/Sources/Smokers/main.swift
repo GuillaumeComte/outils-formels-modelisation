07f825a95a3a56bfecb3b4d7024a456c6d0dedac
Guillaume Comte <32095526+GuillaumeComte@users.noreply.github.com>
2017-10-26 20:48:09 +0200

    Add files via upload

--- tp-02/Smokers/Sources/Smokers/main.swift
+++ tp-02/Smokers/Sources/Smokers/main.swift
@@ -19,10 +19,39 @@ else {
     fatalError("invalid model")
 }
 
+
+
 // Create the initial marking.
 let initialMarking: PTMarking = [r: 1, p: 0, t: 0, m: 0, w1: 1, s1: 0, w2: 1, s2: 0, w3: 1, s3: 0]
 
 // Create the marking graph (if possible).
 if let markingGraph = model.markingGraph(from: initialMarking) {
+    print("Avec comme marquage de départ:\n      ", initialMarking)
+    model.showGraph(i: markingGraph)
     // Write here the code necessary to answer questions of Exercise 4.
+    print(" \n")
+
+
+    // 1. Combien d'états différents votre réseau peut-il avoir ?
+    let nodes = model.countNodes(markingGraph: markingGraph)
+    print("1. Combien d'états différents votre réseau peut-il avoir ?")
+    print("Le réseau peut avoir ",nodes, " états différents.\n")
+
+    // 2. Est-il possible que deux fumeurs différents fument en même temps ?
+    print("2. Est-il possible que deux fumeurs différents fument en même temps ?")
+    let many = model.twoSmokers(m0: markingGraph)
+    if many == true {
+        print("Oui, il est possible que deux fumeurs différents fument en même temps. \n")
+    } else {
+        print("Non, il n'est pas possible que deux fumeurs fument en même temps. \n")
+    }
+
+    // 3. Est-il possible d'avoir deux fois le même ingrédient sur la table ?
+    print("3. Est-il possible d'avoir deux fois le même ingrédient sur la table ?")
+    let ingredient = model.sameIngredient(m0: markingGraph)
+    if ingredient == true {
+        print("Oui, il est possible d'avoir deux fois le même ingrédient sur la table. \n")
+    } else {
+        print("Non, il n'est pas possible d'avoir deux fois le même ingrédient sur la table. \n")
+    }
 }
